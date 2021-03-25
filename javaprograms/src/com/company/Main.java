cef753b1711c7ccaa5f8eecb9ea985c933dcf986
Adnan <adnan.museel123@gmail.com>
2021-03-26 01:39:44 +0500

    java addition program

--- javaprograms/src/com/company/Main.java
+++ javaprograms/src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
+
+        int num1 = 10;
+        int num2 = 20;
+        int result = num1+num2;
+        System.out.println("The sum is: "+result);
     }
 }
