41449b7ff409f72a6fb2c74e221e5fa4e511409e
Adnan <adnan.museel123@gmail.com>
2021-03-26 02:15:48 +0500

    literals in java

--- javaprograms/src/com/company/Main.java
+++ javaprograms/src/com/company/Main.java
@@ -6,9 +6,24 @@ public class Main {
 	// write your code here
         System.out.println("Hello world");
 
+        //sum of two numbers
+/*
         int num1 = 10;
         int num2 = 20;
         int result = num1+num2;
         System.out.println("The sum is: "+result);
+*/
+
+        //literals
+
+        byte age = 32;
+        int age2 = 20;
+        short age3 = 70;
+        long number_Long = 5666882224l;
+        char ch = 'B';
+        float num = 3.6f;
+        double num2 = 4.66d;
+        boolean x = true;
+        System.out.println(ch);
     }
 }
