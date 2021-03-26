eb0c4d3b59a019daf7b4b2308d6211c3d23e0625
Adnan <adnan.museel123@gmail.com>
2021-03-26 05:46:07 +0500

    user input in java

--- javaprograms/src/com/company/Main.java
+++ javaprograms/src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
+//take user input we have to import this module
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
+        //System.out.println("Hello world");
 
         //sum of two numbers
 /*
@@ -15,7 +17,7 @@ public class Main {
 */
 
         //literals
-
+/*
         byte age = 32;
         int age2 = 20;
         short age3 = 70;
@@ -25,5 +27,44 @@ public class Main {
         double num2 = 4.66d;
         boolean x = true;
         System.out.println(ch);
+ */
+
+        //user input
+        //integer value
+/*
+        System.out.println("Taking user input");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first num");
+        int x = sc.nextInt();
+        System.out.println("Enter second num");
+        int y = sc.nextInt();
+        int sum = x+y;
+        System.out.println("The sum of two number is: "+sum);
+
+        //float value
+        System.out.println("Enter float num1");
+        float num1 = sc.nextFloat();
+        System.out.println("enter float num2");
+        float num2 = sc.nextFloat();
+        float res = num1+num2;
+        System.out.println("The result is: "+res);
+
+        //checking datatype of user input
+        System.out.println("write number...");
+        boolean val = sc.hasNextInt();
+        System.out.println(val);
+
+        //print user input
+        String str = sc.nextLine();
+        System.out.println(str);
+
+        //read first word
+        String str2 = sc.next();
+        System.out.println(str2);
+*/
+
+
+
+
     }
 }
